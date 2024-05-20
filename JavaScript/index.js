51571cc36dd66a93c0740f53084a3a1674783049
tufail-khan9 <tufailrose999@gmail.com>
2024-05-20 18:42:29 +0500

    feat:cmnt

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -24,5 +24,40 @@ console.log(firsName[4]);
 var text = "www.tufail.com";
 console.log(text.replace("www.tufail.com", "www.tufailrose.com"));
 
+//some basic maths operation
+var num1 =10;
+var num2 =20;
+var result = num1 + num2;
+console.log(result);
+
+var result2 = num1 - num2;
+console.log(result2);
+
+var result3 = num1 * num2;
+console.log(result3);
+
+var result4 = num1/num2;
+console.log(result4);
+
+//The math objects
+
+var power = Math.pow(10,20);
+console.log(power);
+
+var roundNumber = Math.round(5.8);
+console.log(roundNumber);
+
+var roundNumber2 = Math.ceil(2.4);
+console.log(roundNumber2);
+
+var roundNumber3 = Math.floor(2.4);
+console.log(roundNumber3);
+
+var roundNumber4 = Math.sqrt(4);
+console.log(roundNumber4);
+
+
+
+
 
 
