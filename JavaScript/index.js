54544cc7d8194911853da656baa8a47a48150bb7
tufail-khan9 <tufailrose999@gmail.com>
2024-05-20 18:31:45 +0500

    feat:cmnt

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,28 @@
+
+// write an any text and show in console
 console.log("Hi This Tufail and Want to Learn JavaScript")
+
+// show the data type of string
+var userName = "tufail";
+console.log(typeof userName);
+
+// show the data type of age
+var age =20;
+console.log(typeof age);
+
+//concatinate two words
+var firsName = "Tufail";
+var secondName = "khan";
+var fullName = firsName+ " "+secondName;
+console.log(fullName);
+
+//show of lenght of string and index of string
+console.log(firsName.length);
+console.log(firsName[4]);
+
+//replace a string over another string
+var text = "www.tufail.com";
+console.log(text.replace("www.tufail.com", "www.tufailrose.com"));
+
+
+
